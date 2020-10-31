5b88b34e3e8a7f71cf7492cae699c99017761883
mossyegghead01 <luthfihaidar1176@gmail.com>
2020-10-31 04:19:52 +0000

    website base, cogs loading update, and idk what else

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Ah, ha, ha, ha, stayin' alive, stayin' alive"
+    return render_template('main.html')
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=2333)
 
 def keep_alive():  
     t = Thread(target=run)
