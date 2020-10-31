2a66d8130da4b38863159ddb6fb6ff2dfa4baaa1
mossyegghead01 <luthfihaidar1176@gmail.com>
2020-10-31 04:31:54 +0000

    fixing some port errors

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return render_template('main.html')
 
 def run():
-  app.run(host='0.0.0.0',port=2333)
+  app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():  
     t = Thread(target=run)
